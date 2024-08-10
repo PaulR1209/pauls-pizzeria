4787d87d4083ce29ccd2311702b7ca4e77a2fb10
PaulR1209 Russell <paulr711@gmail.com>
2024-08-10 12:44:28 +0000

    make drop down menu fully funtioning and styled

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,3 +1,5 @@
+// Toogle the navbar menu on mobile
+
 document.addEventListener('DOMContentLoaded', function() {
     const toggler = document.querySelector('.navbar-toggler');
     const menu = document.getElementById('navbarNav');
