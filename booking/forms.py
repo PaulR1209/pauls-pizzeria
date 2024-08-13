81ba4fbf03ba276cce04bf79e9313a1076352482
PaulR1209 Russell <paulr711@gmail.com>
2024-08-13 17:05:22 +0000

    add function to be unable to book past dates and times

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from .models import BookingForm
 from django.utils import timezone
-from datetime import time, timedelta
-from datetime import datetime
+from datetime import time, timedelta, datetime
 
 
 def get_available_time_slots():
@@ -10,8 +9,8 @@ def get_available_time_slots():
     start_time = time(12, 0)
     end_time = time(21, 0)
     time_slots = []
-
     current_time = start_time
+    
     while current_time <= end_time:
         time_slots.append((current_time.strftime('%H:%M'), current_time.strftime('%H:%M')))
         current_time = (datetime.combine(datetime.today(), current_time) + timedelta(minutes=30)).time()
@@ -19,8 +18,15 @@ def get_available_time_slots():
     return time_slots
 
 
-
 class BookingForm(forms.ModelForm):
+
+    def clean_time(self):
+        chosen_time = self.cleaned_data.get('time')
+        current_time = timezone.now().time()
+        if chosen_time < current_time:
+            raise forms.ValidationError("Please select a future time.")
+        return chosen_time
+
     class Meta:
         model = BookingForm
         fields = ['name', 'email', 'phone', 'date', 'time']
