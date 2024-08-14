f6248830d3f0c77213cd23606dcc45931201bf04
PaulR1209 Russell <paulr711@gmail.com>
2024-08-14 11:55:52 +0000

    fixed validation errors for invalid time and date

--- booking/forms.py
+++ booking/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import BookingForm
 from django.utils import timezone
 from datetime import time, timedelta, datetime
-import pytz
 
 
 def get_available_time_slots():
@@ -11,7 +10,6 @@ def get_available_time_slots():
     end_time = time(21, 0)
     time_slots = []
     current_time = start_time
-    pytz.timezone('Europe/London')
     
     while current_time <= end_time:
         time_slots.append((current_time.strftime('%H:%M'), current_time.strftime('%H:%M')))
@@ -25,21 +23,24 @@ class BookingForm(forms.ModelForm):
     def clean_time(self):
         chosen_date = self.cleaned_data.get('date')
         chosen_time = self.cleaned_data.get('time')
-        if chosen_date and chosen_time:
-            combined_datetime = datetime.combine(chosen_date, chosen_time)
-            current_datetime = datetime.now()
-            time_difference = combined_datetime - current_datetime
-            if time_difference.total_seconds() < 0:
-                raise forms.ValidationError("Please select a future time.")
-            return chosen_time
+
+        if not chosen_date:
+            raise forms.ValidationError("This time slot is unavailable.")        
+        
+        combined_datetime = datetime.combine(chosen_date, chosen_time)
+        current_datetime = datetime.now()
+        time_difference = combined_datetime - current_datetime
+        if time_difference.total_seconds() < 0:
+            raise forms.ValidationError("Please select a future time.")
+        return chosen_time
 
     def clean_date(self):
         chosen_date = self.cleaned_data.get('date')
-        if chosen_date:
-            # Get the day of the week (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
-            day_of_week = chosen_date.weekday()
-            if day_of_week in [0, 1]:  # Monday or Tuesday
-                raise forms.ValidationError("We're closed on Mondays and Tuesdays.")
+
+        # Get the day of the week (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+        day_of_week = chosen_date.weekday()
+        if day_of_week in [0, 1]:  # Monday or Tuesday
+            raise forms.ValidationError("We're closed on Mondays and Tuesdays.")
         return chosen_date
 
     class Meta:
