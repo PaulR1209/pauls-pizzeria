47b50cfc1d5f86f289e1524c8bdd879806c0f564
PaulR1209 Russell <paulr711@gmail.com>
2024-08-14 14:56:43 +0000

    add booking assignments onto admin page

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BookingForm, Table
+from .models import BookingForm, Table, BookingAssignment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(BookingForm)
@@ -14,3 +14,13 @@ class PostAdmin(SummernoteModelAdmin):
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('table_number', 'table_capacity', 'is_reserved', 'reserved_by')
+    list_filter = ('table_number', 'table_capacity', 'is_reserved')
+    search_fields = ['table_number', 'table_capacity', 'is_reserved', 'reserved_by']
+
+
+@admin.register(BookingAssignment)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'date', 'time', 'guests', 'table', 'assigned_on')
+    list_filter = ('date', 'time')
+    search_fields = ['name', 'date', 'time', 'table', 'assigned_on']
